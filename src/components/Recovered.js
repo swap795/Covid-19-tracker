fb039a0351964d48c5fd708438fbc08083587ca3
SP <SP>
2020-12-13 20:34:43 -0700

    Merge

---
+++ src/components/Recovered.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import { MenuItem, FormControl, Select } from '@material-ui/core';
+
+function Recovered(countries) {
+   return(
+      <div>
+         Recovered
+      </div>
+   );
+}
+
+export default Recovered;
